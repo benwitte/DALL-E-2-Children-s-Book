dc318be9d50047109739762dd1adbddd3ea1cc08
Benjamin Witte <benjaminwitte@Benjamins-MBP.lan>
2023-07-11 02:18:15 +0300

    dan's fixes

--- src/NewPage.jsx
+++ src/NewPage.jsx
@@ -1,3 +1,53 @@
+
+import { Configuration, OpenAIApi } from "openai";
+import React, { useState } from "react";
+import * as ReactDOM from 'react-dom';
+
+
+const NewPage = (props) => {
+	const configuration = new Configuration({
+		organization: "YOUR_ORG_ID",
+		apiKey: process.env.OPENAI_API_KEY,
+	});
+	const openai = new OpenAIApi(configuration);
+	console.log(openai);
+
+	const [prompt, setPrompt] = useState("Describe Your Monster");
+
+	const apiCall = async () => {
+		// call to openai
+		const result = await openai.completions.create({});
+		return result;
+	};
+
+	const updatePrompt = (e) => {
+		e.preventDefault();
+		setPrompt(e.target.value)
+	}
+
+
+	return (
+		<div>
+			<p>The monster under your bed. What does it look like?</p>
+			<form onSubmit={() => apiCall(prompt)}>
+				<textarea value={prompt} onChange={(e) => updatePrompt(e)} />
+				<input type="textarea" value="Submit" />
+			</form>
+		</div>
+
+	);
+}
+
+
+
+
+
+
+export default NewPage;
+
+
+
+/*
 import { Configuration, OpenAIApi } from "openai";
 import React, { useState } from "react";
 
@@ -27,3 +77,5 @@ const NewPage = (props) => {
 };
 
 export default NewPage;
+
+*/
