7a87fd02178464424dfb0081dd0a6b9b1fd72970
Benjamin Witte <benjaminwitte@Benjamins-MBP.lan>
2023-07-12 18:06:17 +0300

    hooked up with api, prompts in place

--- src/NewPage.jsx
+++ src/NewPage.jsx
@@ -3,20 +3,39 @@ import { Configuration, OpenAIApi } from "openai";
 import React, { useState } from "react";
 import * as ReactDOM from 'react-dom';
 
-
 const NewPage = (props) => {
 	const configuration = new Configuration({
-		organization: "YOUR_ORG_ID",
-		apiKey: process.env.OPENAI_API_KEY,
+		organization: "FILL THIS IN",
+		apiKey: "FILL THIS IN"
 	});
 	const openai = new OpenAIApi(configuration);
-	console.log(openai);
+
 
 	const [prompt, setPrompt] = useState("Describe Your Monster");
 
-	const apiCall = async () => {
+	const [monsterName, setMonsterName] = useState("")
+
+
+	// creates list of descriptions sent to api
+	const [imgURL, setImageURL] = useState([]);
+
+	const imageSequences = ["", " at the beach, building a sand castle, wearing lei, medium shot", 
+	" in Egypt, pyramid, walking like an Egyptian, wearing pharaoh hat",
+" in Paris, eating croissant, wearing beret, eiffel tower",
+"in bedroom, wearing pharaoh hat, eating croissant, wearing lei, happy, upbeat, friendly"]
+
+
+	const apiCall = async (e, prompt, imgSqnce) => {
 		// call to openai
-		const result = await openai.completions.create({});
+		e.preventDefault();
+		const result = await openai.createImage({
+			prompt: "cute monster " + prompt + ", impressionist, pointilism, artistic, cute aesthetic," + imgSqnce,
+			n: 1,
+			size: "1024x1024",
+		});
+		const url = result.data.data[0].url;
+		setImageURL([...imgURL, url])
+		console.log(imgURL);
 		return result;
 	};
 
@@ -25,14 +44,46 @@ const NewPage = (props) => {
 		setPrompt(e.target.value)
 	}
 
+	const updateMonsterName = (e) => {
+		e.preventDefault();
+		setMonsterName(e.target.value)
+	}
+
+
 
 	return (
 		<div>
-			<p>The monster under your bed. What does it look like?</p>
-			<form onSubmit={() => apiCall(prompt)}>
-				<textarea value={prompt} onChange={(e) => updatePrompt(e)} />
-				<input type="textarea" value="Submit" />
-			</form>
+			<p>Do you have a monster under your bed? What is its name?</p>
+			<input name = "Name" value={monsterName} onChange={(e) => updateMonsterName(e)} />
+			<br></br>
+			<p>What does {monsterName} look like?</p>
+			<textarea value={prompt} onChange={(e) => updatePrompt(e)} />
+			<br></br>
+			<button onClick={(e) => apiCall(e, prompt, imageSequences[0])}> submit </button>
+			<img src={imgURL[0]}></img>
+
+			<p> One day, {monsterName} was tired of scaring people.  {monsterName} was tired of making scary faces and scary noises.  {monsterName}  just wanted to have fun.
+			So it decided to take a vacation.</p>
+			
+			<p> First, {monsterName} went to Hawaii and made sand castles</p>
+			<button onClick={(e) => apiCall(e, prompt, imageSequences[1])}> submit </button>
+			<img src={imgURL[1]}></img>
+
+
+			<p> Next, {monsterName} went to Egypt and walked like an Egyptian!</p>
+			<button onClick={(e) => apiCall(e, prompt, imageSequences[2])}> submit </button>
+			<img src={imgURL[2]}></img>
+
+			<p> Next, {monsterName} went to France for some joi de vivre!</p>
+			<button onClick={(e) => apiCall(e, prompt, imageSequences[3])}> submit </button>
+			<img src={imgURL[3]}></img>
+
+			<p> When {monsterName} came home at last, it was very happy, and {monsterName} never scared anyone again. </p>
+			<button onClick={(e) => apiCall(e, prompt, imageSequences[4])}> submit </button>
+			<img src={imgURL[4]}></img>
+
+
+
 		</div>
 
 	);
@@ -45,37 +96,3 @@ const NewPage = (props) => {
 
 export default NewPage;
 
-
-
-/*
-import { Configuration, OpenAIApi } from "openai";
-import React, { useState } from "react";
-
-const NewPage = (props) => {
-	const configuration = new Configuration({
-		organization: "YOUR_ORG_ID",
-		apiKey: process.env.OPENAI_API_KEY,
-	});
-	const openai = new OpenAIApi(configuration);
-	console.log(openai);
-
-	const [prompt, setPrompt] = useState("");
-
-	const apiCall = async () => {
-		// call to openai
-		const result = await openai.completions.create({});
-		return result;
-	};
-
-	return (
-		<div>
-			<p>Hi Ben</p>
-			<p>{prompt}</p>
-			<button onSubmit={() => apiCall(prompt)}>Submit</button>
-		</div>
-	);
-};
-
-export default NewPage;
-
-*/
